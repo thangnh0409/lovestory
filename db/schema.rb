efba143800c5adc0a7cdfb8dcd5399a7a0fe8a19
thangnh <thang@camobile.com>
2015-10-13 19:57:28 +0900

    account activation + password reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151009082811) do
+ActiveRecord::Schema.define(version: 20151013071933) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "email",           limit: 255
-    t.string   "password_digest", limit: 255
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
-    t.string   "remember_digest", limit: 255
-    t.boolean  "admin",           limit: 1,   default: false
+    t.string   "name",              limit: 255
+    t.string   "email",             limit: 255
+    t.string   "password_digest",   limit: 255
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
+    t.string   "remember_digest",   limit: 255
+    t.boolean  "admin",             limit: 1,   default: false
+    t.string   "activation_digest", limit: 255
+    t.boolean  "activated",         limit: 1,   default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest",      limit: 255
+    t.datetime "reset_sent_at"
   end
 
 end
